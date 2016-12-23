10405a2ab64b2dd6603c5b61a98f16f19d32cad7
Mingxing LAI <me@mingxinglai.com>
2016-12-23 16:54:04 +0800

    获取请求参数

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, redirect
+from flask import Flask, url_for, redirect, request
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -14,10 +14,11 @@ def hello():
 def about():
     return redirect(url_for('static', filename='style.css'))
 
-@app.route('/user/<username>')
+@app.route('/user/<username>', methods=['GET', 'POST'])
 def show_user_profile(username):
     # show the user profile for that user
-    return 'User %s' % username
+    age = request.args.get('age', -1)
+    return 'HTTP method : {0} User {1} age {2}'.format(request.method, username, age)
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
